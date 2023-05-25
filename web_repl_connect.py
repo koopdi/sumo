3f4bac25068a21a3135b1017482d12eb89889e37
koopdi <movonarvic@gmail.com>
2023-05-25 15:58:41 -0700

    Header comment, web_repl_connect.py

--- web_repl_connect.py
+++ web_repl_connect.py
@@ -1,2 +1,6 @@
+# Start the webrepl for remote coding.
+# This file should be imported in boot.py, after setting up networking.
+# Thonny doesn't have an option for pico w, so choose esp8266 instead and configure the connection for webrepl instead of the com port.
+
 import webrepl
 webrepl.start(password="1234")
