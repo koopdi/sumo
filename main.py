2f3d1fe2ebd86e2b9e0bc18c1951bbd87d8ffc3c
koopdi <movonarvic@gmail.com>
2023-05-25 15:37:41 -0700

    comment main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+# This code will run automatically after boot.py.
+
 import uos
 print("System Information")
 # display system information
 print('uname: ', uos.uname())
-
-
 
 import sumo
 print("Launching sumo code...")
