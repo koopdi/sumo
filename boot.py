3ff736419e86993c210947947cacbe3142b62aa4
koopdi <movonarvic@gmail.com>
2023-05-25 15:36:04 -0700

    Comments in boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,9 @@
+# This code will run automatically before main.py
+
+# Signal a successful boot by printing a message and blinking the onboard LED. 
 print('boot.py')
 import blink_pico_w
+
+# Create a wifi access point and start the webrepl.
 import start_AP_IF
 import web_repl_connect
