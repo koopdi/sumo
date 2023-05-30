402da92aa8251ff916d50cdb73151772412b3075
Marshal Banana <movonarvic@gmail.com>
2023-05-29 22:26:27 -0700

    a class to handle the hbridge stuff

---
+++ motor_driver.py
@@ -0,0 +1,17 @@
+"This program uses the Sparkfun TB6612FNG motor driver"
+class TB66:
+    def __init__(self, ina1 = None, ina2 = None, pwma = None,
+                       inb1 = None, inb2 = None, pwmb = None):
+        pass
+
+    # motor a
+    ina1 = None
+    ina2 = None
+    pwma = None
+
+    # motor b
+    inb1 = None
+    inb2 = None
+    pwmb = None
+
+
