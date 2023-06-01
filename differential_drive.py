35c361c046def53d71ae8b8372cb39c7d9dbf048
Marshal Banana <movonarvic@gmail.com>
2023-05-31 20:55:27 -0700

    h-bridge pin setup copied from sumo

--- differential_drive.py
+++ differential_drive.py
@@ -3,6 +3,27 @@ from machine import Pin ,PWM
 from utime import sleep
 
 DEFAULT_SPEED = 30
+
+# motor a control pins
+ina1 = Pin(18, Pin.OUT)
+ina2 = Pin(17, Pin.OUT)
+
+# motor b control pins
+inb1 = Pin(19, Pin.OUT)
+inb2 = Pin(20, Pin.OUT)
+
+# speed control pins
+pwma = PWM(Pin(16)) # motor a
+pwmb = PWM(Pin(21)) # motor b
+
+# set pwm frequency
+pwma.freq(20000)
+pwmb.freq(20000)
+
+# set pwm duty to 0 at startup
+pwma.duty_u16(0)
+pwmb.duty_u16(0)
+
 class Diff_Drive():
     def forward():
         pass
