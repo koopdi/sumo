5875ef2680c0563d1ab3a01bf14747703405954f
Marshal Banana <movonarvic@gmail.com>
2023-06-02 15:41:28 -0700

    left/right test

--- differential_drive.py
+++ differential_drive.py
@@ -89,7 +89,9 @@ drive = Diff_Drive(bridge)
 def test():
     bridge = motor_driver.TB66(ina1, ina2, pwma, inb1, inb2, pwmb)
     drive = Diff_Drive(bridge)
-    sleep(3)
+    for speed in range(0,101,1):
+        drive.left(speed = speed, time = 0.1)
+        drive.right(speed = speed, time = 0.1)
     for speed in range(0,101,1):
         drive.forward(speed = speed, time = 0.1)
         drive.reverse(speed = speed, time = 0.1)
