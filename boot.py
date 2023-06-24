16a90865b7407c286fdc9ea5bac6492bde6d1953
Marshal Banana <movonarvic@gmail.com>
2023-06-23 21:32:25 -0700

    esp32 boot (ok for pico too)

--- boot.py
+++ boot.py
@@ -1,9 +1,29 @@
-# This code will run automatically before main.py
+# This file is executed on every boot (including wake-boot from deepsleep)
 
-# Signal a successful boot by printing a message and blinking the onboard LED. 
-print('boot.py')
-import blink_pico_w
+# doesn't seem to do anythin
+# import esp
+# esp.osdebug(esp.LOG_VERBOSE) #os debug on
+# esp.osdebug(None) #os debug off
 
-# Create a wifi access point and start the webrepl.
-import start_AP_IF
-import web_repl_connect
+# copy log from previous boot then clear it
+# import os
+# os.rename("log.txt", "log.old.txt")
+
+import logger
+log = logger.init("boot")
+log.info(" ---------------- system online -------------------")
+logger.flush()
+
+#---------------setup webrepl---------------------------#
+# import network
+# 
+# ap = network.WLAN(network.AP_IF)
+# ap.active(True)
+# ap.config(essid='bobby_drop_tables', password='smith')
+# 
+# import webrepl
+# webrepl.start(password="1234")
+
+#----------calibrate esc----#
+# from esc import calibrate
+# calibrate()
