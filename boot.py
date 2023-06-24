71afb2825ce95a05fb8eef7609eb31191c6663d3
Marshal Banana <movonarvic@gmail.com>
2023-06-23 21:41:27 -0700

    remove unused code

--- boot.py
+++ boot.py
@@ -1,14 +1,5 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 
-# doesn't seem to do anythin
-# import esp
-# esp.osdebug(esp.LOG_VERBOSE) #os debug on
-# esp.osdebug(None) #os debug off
-
-# copy log from previous boot then clear it
-# import os
-# os.rename("log.txt", "log.old.txt")
-
 import logger
 log = logger.init("boot")
 log.info(" ---------------- system online -------------------")
@@ -27,7 +18,3 @@ led.freq(20)
 # 
 # import webrepl
 # webrepl.start(password="1234")
-
-#----------calibrate esc----#
-# from esc import calibrate
-# calibrate()
