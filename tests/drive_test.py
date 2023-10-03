209f35feb85d7b6ed244714debd3c218e25944b0
Marshal Banana <movonarvic@gmail.com>
2023-10-03 16:17:47 -0700

    testing new drive code

--- tests/drive_test.py
+++ tests/drive_test.py
@@ -1,9 +1,19 @@
 from time import sleep
 
 import drive
+from blink_pwm import led
+led.duty_u16(0)
 
 SPEED = 20
 
 drive.right(speed=SPEED, time=0.2)
 sleep(0.5)
 drive.left(speed=SPEED, time=0.2)
+sleep(0.5)
+
+drive.head(720 - 90)
+print("heading on course")
+sleep(3)
+drive.head(-45)
+
+# drive.turn(angle = -45)
